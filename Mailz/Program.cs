786251e7752765a60dd23b7fa8e003f0fe947077
Mohammad s. Albay <MohammadAlbay99@gmail.com>
2026-02-06 16:37:37 +0200

    init-2.06

--- Mailz/Program.cs
+++ Mailz/Program.cs
@@ -6,10 +6,13 @@ using ITStage.Mail.IMAP;
 Console.WriteLine("Starting Mailz Unified Mail Server...");
 var config = UnifiedMailServerConfig.LoadConfig("/etc/mailz/config/ums.json");
 
-_ = Task.Run(async () =>
+
+Task.WaitAll([
+    Task.Run(async () =>
 {
     IMAPServer imapServer = new IMAPServer(config);
     await imapServer.Initialize();
     await imapServer.Connect();
-});
+})
+]);
 Console.WriteLine("Hello, World! Build Success!");
